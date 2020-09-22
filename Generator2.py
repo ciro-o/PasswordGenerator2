b906a651c810d8fca15ae3e6f9a0969747fb26c1
ciro-o <52354311+ciro-o@users.noreply.github.com>
2020-09-22 11:14:16 -0300

    Add files via upload

---
+++ Generator2.py
@@ -0,0 +1,193 @@
+import random
+import string 
+from colorama import init, Fore
+
+init(autoreset=True)
+
+print(Fore.RED + "//////////////////////////////////////")
+print(Fore.RED + "//                                  //")
+print(Fore.RED + "//           -PASSWORD-             //")
+print(Fore.RED + "//           -GENERATOR-            //")
+print(Fore.RED + "//           Version 2.0            //")
+print(Fore.RED + "//                                  //")
+print(Fore.RED + "//          Por: Ciro-o             //")
+print(Fore.RED + "//    https://github.com/ciro-o     //")
+print(Fore.RED + "//                                  //")
+print(Fore.RED + "//////////////////////////////////////")
+
+
+Contraseña1 = random.choice(string.digits)
+
+Contraseña2 =  (random.choice(string.digits) + random.choice(string.ascii_letters))
+
+Contraseña3 =  (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits))
+
+Contraseña4 =  (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters))
+
+Contraseña5 =  (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits))
+
+Contraseña6 =  (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters))
+
+Contraseña7 =  (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits))
+
+Contraseña8 =  (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters))
+
+Contraseña9 =  (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits))
+
+Contraseña10 = (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters))
+
+Contraseña11 = (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits))
+
+Contraseña12 = (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters))
+
+Contraseña13 = (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits))
+ 
+Contraseña14 = (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters))
+ 
+
+Contraseña15 = (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits))
+
+Contraseña16 = (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters))
+
+Contraseña17 = (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits))
+
+Contraseña18 = (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters))
+
+Contraseña19 = (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits))
+
+Contraseña20 = (random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) + 
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters) +
+ 				random.choice(string.digits) + random.choice(string.ascii_letters))
+
+llave = int(input("Escribe la cantidad de caracteres para tu contraseña: "));
+
+if llave == 1:
+	print(Contraseña1)
+elif llave == 2:
+	print(Contraseña2)
+elif llave == 3:
+	print(Contraseña3)
+elif llave == 4:
+	print(Contraseña4)
+elif llave == 5:
+	print(Contraseña5)
+elif llave == 6:
+	print(Contraseña6)
+elif llave == 7:
+	print(Contraseña7)
+elif llave == 8:
+	print(Contraseña8)
+elif llave == 9:
+	print(Contraseña9)
+elif llave == 10:
+	print(Contraseña10)
+elif llave == 11:
+	print(Contraseña11)
+elif llave == 12:
+	print(Contraseña12)
+elif llave == 13:
+	print(Contraseña13)
+elif llave == 14:
+	print(Contraseña14)
+elif llave == 15:
+	print(Contraseña15)
+elif llave == 16:
+	print(Contraseña16)
+elif llave == 17:
+	print(Contraseña17)
+elif llave == 18:
+	print(Contraseña18)
+elif llave == 19:
+	print(Contraseña19)
+elif llave == 20:
+	print(Contraseña20)
+else:
+	print("No hay mas de 20 =)")
